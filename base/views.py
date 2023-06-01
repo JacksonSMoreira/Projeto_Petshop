3ade99062a3a9f9dd6cea5c304e4c23ac6a8f413
Jackson-moreira-DevPrograma <ribsjackson@gmail.com>
2023-06-01 21:05:15 +0100

    carregando bootstra5

--- base/views.py
+++ base/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from base.forms import ContatoForm
 
 def inicio(request):
     return render(request,'inicio.html')
 
 def contato(request):
+    if request.method =='GET':
+        form = ContatoForm()
+    else:
+        form = ContatoForm(request.POST)
     contexto = {
-        'telefone': '(99) 9999.9999',
-        'responsavel': 'Mario da Silva'
+        'telefone': '(351) 914 505 598/PT ou chame whatsapp.',
+        'responsavel': 'Jackson Moreira',
+        'form': form
     } 
     return render(request, 'contato.html', contexto)
 
--- ultima/settings.py
+++ ultima/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'base',
 ]
 
@@ -118,9 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    'static/',
-]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
