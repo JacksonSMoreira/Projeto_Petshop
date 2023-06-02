ed7ab6143de1a37264dcc71620acfe143c4ac023
Jackson-moreira-DevPrograma <ribsjackson@gmail.com>
2023-06-02 22:35:48 +0100

    atualizando tag forms

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField()
-    email = forms.EmailField()
+    nome = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=11)
     mensagem = forms.CharField(widget=forms.Textarea)
